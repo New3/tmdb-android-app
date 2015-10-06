979d088ad3f98da9a00badd3817e5965c186414d
Jose 0. Mateo Acosta <jose.mateoacosta@dominionenterprises.com>
2015-10-06 11:06:57 -0400

    added movieadapter to show posters using picasso

--- app/src/main/java/com/mateoj/popularmoviesdemo/MainActivity.java
+++ app/src/main/java/com/mateoj/popularmoviesdemo/MainActivity.java
@@ -1,15 +1,26 @@
 package com.mateoj.popularmoviesdemo;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private MoviesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +29,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mAdapter = new MoviesAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        List<Movie> movies = new ArrayList<>();
+
+        for (int i = 0; i < 25; i++) {
+            movies.add(new Movie());
+        }
+        mAdapter.setMovieList(movies);
+
     }
 
     @Override
@@ -48,5 +62,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+    public static class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+        private List<Movie> mMovieList;
+        private LayoutInflater mInflater;
+        private Context mContext;
+
+        public MoviesAdapter(Context context) {
+            this.mContext = context;
+            this.mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = mInflater.inflate(R.layout.row_movie, parent, false);
+            MovieViewHolder viewHolder = new MovieViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(MovieViewHolder holder, int position) {
+            Movie movie = mMovieList.get(position);
+            Picasso.with(mContext)
+                    .load(movie.getPoster())
+                    .placeholder(R.color.colorAccent)
+                    .into(holder.imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return (mMovieList == null) ? 0 : mMovieList.size();
+        }
+
+        public void setMovieList(List<Movie> movieList) {
+            this.mMovieList = new ArrayList<>();
+            this.mMovieList.addAll(movieList);
+            notifyDataSetChanged();
+        }
     }
 }
